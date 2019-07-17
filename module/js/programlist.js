199125c651b6c307004531634fc2783abf44f56a
Ap0stader <ap0stader@outlook.com>
2019-07-17 20:48:08 +0800

    Prefect module PROGRAMLIST

--- module/js/programlist.js
+++ module/js/programlist.js
@@ -32,19 +32,33 @@ var Swiper = new Swiper('#swiper', {
     }
 });
 
+// 给定一个大于swiper-slide的数字，它会跳回第一张
+var lastnow = 20;
+var lastoriginal = false;
 
-// $.ajax({
-//     url: 'programlist.json',
-//     dataType: "json",
-//     cache: false,
-//     success: function (data) {
-//         i = data.current;
-//         if (i >= 1) {
-//             var str = "#s" + i;
-//             $(str).attr("class", "highlight");
-//             $(str).append("<span class='now'>正在上演</span>");
-//             console.log($(str).index("section"));
-//             Swiper.slideTo($(str).index("section"));
-//         }
-//     }
-// });
+function querynow(){
+    $.ajax({
+        url: '/config/livestatus.json',
+        dataType: "json",
+        cache: false,
+        success: function (data) {
+            // 更新的条件，有正确的值且不等于之前的值
+            if (data.Current >= 0 && data.Current != lastnow) {
+                // 1.恢复之前节目本来的class
+                lastoriginal == true ? $("#s" + lastnow + " h2").attr("class", "original") : $("#s" + lastnow + " h2").attr("class", " ");
+                // 2.保存当前节目本来的class
+                lastnow = data.Current;
+                $("#s" + data.Current + " h2").attr("class") == undefined ? lastoriginal = false : lastoriginal = true;
+                // 3.更改当前节目的class
+                $("#s" + data.Current + " h2").attr("class", "now");
+                // 4.Swiper切换至当前的节目
+                // https://www.swiper.com.cn/api/methods/109.html
+                Swiper.slideTo($("#s" + data.Current).index());
+            }
+        }
+    });
+}
+
+// TODO 此处只是权宜之计
+// 每3秒钟查询一次当前的节目
+setInterval(querynow, 3000);
