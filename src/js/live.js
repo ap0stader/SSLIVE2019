d3b3825bfa1c1e927da89662a134f4c0e8e34516
ap0stader <ap0stader@outlook.com>
2019-08-06 22:20:26 +0800

    Update LIVE, Only Use HLS, Clean Project

--- src/js/live.js
+++ src/js/live.js
@@ -1,7 +1,7 @@
 var videowidth, videoheight;
 
 function resize() {
-    if($(window).width() < 750){
+    if ($(window).width() < 750) {
         // 根据屏幕宽度更改文字，以保证文字在一行内显示
         $('#title').text('省实“青春诗会”直播')
         $('html').css('font-size', $(window).width() / 31.25 + 'px');
@@ -11,7 +11,7 @@ function resize() {
         $('#video').css('height', videoheight);
         $('#video').css('max-width', videowidth);
         $('#video').css('min-height', videoheight);
-    }else{
+    } else {
         // 根据屏幕宽度更改文字，以保证文字在一行内显示
         $('#title').text('广东实验中学“青春诗会”直播');
         videowidth = $(window).width() * 0.618 > 750 ? $(window).width() * 0.618 : 750;
@@ -28,77 +28,31 @@ function resize() {
     $('body').css('width', $(window).width());
 }
 
-var playmode;
-var flvplayer, hlsplayer;
-function load(){
-    if(!flvjs.isSupported()){
-        var playmode = 'FLV';
-        flvplayer = flvjs.createPlayer({
-            type : 'flv',
-            isLive : true,
-            hasAudio: true,
-            hasVideo: true,
-            cors: true,
-            url: "http://suit.ssersay.cn/SUIT/stream.flv"
-        },{
-            enableStashBuffer: true,
-        });
-        var flvElement = document.getElementById('video');
-        flvplayer.attachMediaElement(flvElement);
-        flvplayer.load();
-    }else{
-        var playmode = 'HLS';
-        $('#video').addClass("video-js vjs-big-play-centered");
-        hlsplayer = videojs('video', {
-            liveui: true,
-            controls: false,
-            preload: 'auto',
-        });
-        hlsplayer.src("http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8");
-    }
+var player;
+function load() {
+    player = videojs('video', {
+        liveui: true,
+        controls: false,
+        preload: true,
+    });
+
+    player.src("https://suit.ssersay.cn/SUIT/stream.m3u8");
 
     var isplay = false;
-    $('#play').click(function(){
-        if(isplay){
-            if(playmode == 'FLV'){
-                flvplayer.pause();
-            }else if(playmode == 'HLS'){
-                hlsplayer.pause();
-            }
+    $('#play').click(function () {
+        if (isplay) {
+            player.pause();
             $('#play').css('background-image', "url('/src/img/play.svg')");
             isplay = false;
-        }else{
-            if(playmode == 'FLV'){
-                flvplayer.play();
-            }else if(playmode == 'HLS'){
-                hlsplayer.play();
-            }
+        } else {
+            player.play();
             $('#play').css('background-image', "url('/src/img/pause.svg')");
             isplay = true;
         }
     });
 
-    $('#fullscreen').click(function(){
-        if(playmode == 'FLV'){
-            var ele = document.getElementById("video");
-            if (ele.requestFullscreen) {
-                ele.requestFullscreen();
-            }
-            else if (ele.webkitRequestFullscreen) {
-                ele.webkitRequestFullscreen();
-            }
-            else if (ele.msRequestFullscreen) {
-                ele.msRequestFullscreen();
-            }
-            else if (ele.mozRequestFullScreen) {
-                ele.mozRequestFullScreen();
-            }
-            else if (ele.oRequestFullScreen) {
-                ele.oRequestFullScreen();
-            }
-        }else if(playmode == 'HLS'){
-            hlsplayer.requestFullscreen();
-        }
+    $('#fullscreen').click(function () {
+        player.requestFullscreen();
     });
 
     resize();
