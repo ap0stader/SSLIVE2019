23fe421592c869a3a89819b5f6a4baf57eae9018
ap0stader <ap0stader@outlook.com>
2019-08-08 21:41:21 +0800

    Update BACKEND

--- console/livestatus.php
+++ console/livestatus.php
@@ -1,3 +1,16 @@
 <?php
-
-echo "success";
+// 获取POST中的cmd数据
+$cmd = $_POST["cmd"];
+// 读取livestatus.json文件
+$json_string = file_get_contents("../config/livestatus.json");
+// 将JSON转化为PHP对象
+$data = json_decode($json_string);
+// 为Live赋值
+$data->Live = ($cmd == "start");
+// 将PHP对象转化为JSON
+$json_strings = json_encode($data);
+// 写入livestatus.json文件
+$code = file_put_contents("../config/livestatus.json", $json_strings);
+if ($code > 0) {
+    echo "success";
+}
