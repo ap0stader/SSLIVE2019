fba4d2b0e95baac35bf810b3f0e086fe355c891e
ap0stader <ap0stader@outlook.com>
2019-08-08 21:46:24 +0800

    Perfect BACKEND

--- src/js/live.js
+++ src/js/live.js
@@ -36,6 +36,8 @@ function load() {
         preload: true,
     });
 
+    // TODO 更改为真实的地址
+    // player.src("https://suit.ssersay.cn/SUIT/stream.m3u8");
     player.src("https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8");
 
     var isplay = false;
