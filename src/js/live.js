f70fbc8f60e18d1ce50dcdf7874c6ecd57d08d67
ap0stader <ap0stader@outlook.com>
2019-08-08 20:02:35 +0800

    Update BACKEND

--- src/js/live.js
+++ src/js/live.js
@@ -36,7 +36,7 @@ function load() {
         preload: true,
     });
 
-    player.src("https://suit.ssersay.cn/SUIT/stream.m3u8");
+    player.src("https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8");
 
     var isplay = false;
     $('#play').click(function () {
