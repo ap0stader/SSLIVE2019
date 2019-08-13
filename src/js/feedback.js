3d9185ff69c44c7d73e35b65ea13fd6ebb229f35
ap0stader <ap0stader@outlook.com>
2019-08-13 14:23:10 +0800

    Update FEEDBACK

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,4 +1,15 @@
 // feedback.js
+function browserjs_agree() {
+    $("#browserjs-disagree").css("display", "none");
+    $("#browserjs-disagree-notice").css("display", "none");
+    $("#browserjs-agree").text("已同意使用这些数据");
+    $("#step2").css("display", "block");
+}
+
+function browserjs_disagree() {
+    $("#browserjs-disagree-notice").css("display", "block");
+}
+
 // 调整页面的字体大小
 var rem = 16;
 function resize() {
@@ -11,6 +22,8 @@ function resize() {
 }
 
 function load() {
+    $("#browserjs-agree").click(browserjs_agree);
+    $("#browserjs-disagree").click(browserjs_disagree);
     resize();
 }
 
