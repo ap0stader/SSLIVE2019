4560375c83d49b222b12692d6c668cf28479644d
ap0stader <ap0stader@outlook.com>
2019-08-15 22:20:08 +0800

    已实现console发送弹幕

--- src/js/live.js
+++ src/js/live.js
@@ -12,6 +12,17 @@ function load() {
     // TODO 更改为真实的地址
     // player.src("https://suit.ssersay.cn/SUIT/stream.m3u8");
     player.src("https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8");
+
+    $("#danmu").danmu({
+        height: $("#video_html5_api").height(),  //弹幕区高度
+        width: $("#video_html5_api").width(),   //弹幕区宽度
+        zindex: 50,
+        fontSizeSmall: 1.5 * parseInt($('html').css('font-size')),     //小弹幕的字号大小
+        FontSizeBig: 2 * parseInt($('html').css('font-size')),       //大弹幕的字号大小
+
+        positionOptimize: true,         //是否位置优化，位置优化是指像AB站那样弹幕主要漂浮于区域上半部分
+    });
+    $('#danmu').danmu('danmuStart');
 }
 
 $('document').ready(load)
--- src/util/danmu.js
+++ src/util/danmu.js
@@ -3,10 +3,13 @@
 create_socket();
 
 function add_danmu(text, color, size, position) {
-    console.log(text + color + size + position)
+    var time = $('#danmu').data("nowTime") + 1;
+    $("#danmu").danmu("addDanmu", [
+        { "text": text, "color": color, "size": size, "position": position, "time": time }
+    ]);
 }
 
-// send_danmu("Hello", "Red", "Big", "Center")
+// send_danmu("Hello", "white", 1, 0)
 function send_danmu(text, color, size, position) {
     // 合成data字符串
     var data_obj = '{"text":"' + text + '","color":"' + color + '","size":"' + size + '","position":"' + position + '"}';
