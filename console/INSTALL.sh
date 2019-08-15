0d092a8a2c707c07ba9efd7f4bb9e9bfd9c3626f
ap0stader <ap0stader@outlook.com>
2019-08-15 16:06:18 +0800

    添加了服务器的测试

--- console/INSTALL.sh
+++
@@ -1,8 +0,0 @@
-# use LAMP installer
-apt-update
-apt-get -y install wget screen git
-git clone https://github.com/teddysun/lamp.git
-cd lamp
-chmod 755 *.sh
-screen -S lamp
-./lamp.sh
--- console/danmu/danmu_server.php
+++ console/danmu/danmu_server.php
@@ -1,42 +1,48 @@
 <?php
-// 创建websocket服务器对象
-// 监听0.0.0.0:9505端口
-$serv = new swoole_websocket_server("0.0.0.0", 9505, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
-// 配置证书文件位置
-$key_dir = "./cert/";
-$serv->set(
+// 创建websocket服务器对象，监听0.0.0.0:9505端口
+// https://wiki.swoole.com/wiki/page/14.html
+$websocket = new swoole_websocket_server("0.0.0.0", 9505, SWOOLE_PROCESS, SWOOLE_TCP | SWOOLE_SSL);
+
+// 配置证书文件
+// https://wiki.swoole.com/wiki/page/274.html
+$websocket->set(
     array(
-        'work_num' => 1,
-        'ssl_cert_file' => $key_dir . 'backend.crt',
-        'ssl_key_file' => $key_dir . 'backend.key'
+        // https://wiki.swoole.com/wiki/page/278.html
+        'daemonize' => 1,
+        // https://wiki.swoole.com/wiki/page/318.html
+        'ssl_cert_file' => './cert/cert.pem',
+        'ssl_key_file' => './cert/cert.key',
+        // https://wiki.swoole.com/wiki/page/280.html
+        'log_file' => './log/log.log'
     )
 );
+
 //监听WebSocket连接打开事件
-$serv->on('open', function ($serv, $request) {
-    //var_dump($request->fd, $request->get, $request->server);
-    //相当于记录一个日志吧，有连接时间和连接ip
-    echo $request->fd . '-----time:' . date("Y-m-d H:i:s", $request->server['request_time']) . '--IP--' . $request->server['remote_addr'] . '-----';
+$websocket->on('open', function ($websocket, $request) {
+    // //var_dump($request->fd, $request->get, $request->server);
+    // //相当于记录一个日志吧，有连接时间和连接ip
+    // echo $request->fd . '-----time:' . date("Y-m-d H:i:s", $request->server['request_time']) . '--IP--' . $request->server['remote_addr'] . '-----';
 });
 
 //监听WebSocket消息事件
-$serv->on('message', function ($serv, $frame) {
-    //记录收到的消息，可以写到日志文件中
-    echo "Message: {$frame->data}\n";
+$websocket->on('message', function ($websocket, $frame) {
+    // //记录收到的消息，可以写到日志文件中
+    // echo "Message: {$frame->data}\n";
 
-    //遍历所有连接，循环广播
-    foreach ($serv->connections as $fd) {
-        //如果是某个客户端，自己发的则加上isnew属性，否则不加
-        if ($frame->fd == $fd) {
-            $serv->push($frame->fd, $frame->data . ',"isnew":""');
-        } else {
-            $serv->push($fd, "{$frame->data}");
-        }
-    }
+    // //遍历所有连接，循环广播
+    // foreach ($websocket->connections as $fd) {
+    //     //如果是某个客户端，自己发的则加上isnew属性，否则不加
+    //     if ($frame->fd == $fd) {
+    //         $websocket->push($frame->fd, $frame->data . ',"isnew":""');
+    //     } else {
+    //         $websocket->push($fd, "{$frame->data}");
+    //     }
+    // }
 });
 
 //监听WebSocket连接关闭事件
-$serv->on('close', function ($serv, $fd) {
-    echo "client-{$fd} is closed\n";
+$websocket->on('close', function ($websocket, $fd) {
+    // echo "client-{$fd} is closed\n";
 });
 
-$serv->start();
+$websocket->start();
