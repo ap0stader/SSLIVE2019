d644f0e437ca61609edc68a8ea3345efe9252d14
ap0stader <ap0stader@outlook.com>
2019-09-24 13:16:48 +0800

    增加了适配bilibili直播的相关代码

--- console/livestatus.php
+++ console/livestatus.php
@@ -2,7 +2,7 @@
 // 获取POST中的cmd数据
 $cmd = $_POST["cmd"];
 // 读取livestatus.json文件
-$json_string = file_get_contents("../config/livestatus.json");
+$json_string = file_get_contents("./livestatus.json");
 // 将JSON转化为PHP对象
 $data = json_decode($json_string);
 // 为Live赋值
--- src/js/introduction.js
+++ src/js/introduction.js
@@ -3,17 +3,54 @@
 // 判断是否已经开播
 function checker() {
     $.ajax({
-        url: "/config/livestatus.json",
+        url: "/config/livemode.json",
         method: "GET",
         success: function (data) {
-            var live = data.Live;//是否开播
+            var mode = data.mode;//是否开播
             // 分发到直播页面条件：JSON文件中直播已经开始
-            if (live) {
-                window.location.href = 'live.html'
+            if (mode == 'ssersay') {
+                $.ajax({
+                    url: "/console/livestatus.json",
+                    method: "GET",
+                    success: function (data) {
+                        var live = data.Live;//是否开播
+                        // 分发到直播页面条件：JSON文件中直播已经开始
+                        if (live) {
+                            window.location.href = 'live.html'
+                        } else {
+                            window.location.href = 'introduction.html';
+                        }
+                    },
+                    error: function () {
+                        $('#loading').text(" ")
+                        $("#errmsg").text("请检查网络链接并刷新页面");
+                    }
+                });
+            } else if (mode == 'bilibili') {
+                $.ajax({
+                    url: "/config/livetime.json",
+                    method: "GET",
+                    success: function (data) {
+                        var nowtime = new Date().getTime();//当前时间
+                        var livetime = new Date(data.year, data.month - 1, data.day, data.hour, data.minute).getTime();//开播时间
+                        var leftTime = livetime - nowtime;//计算时差
+                        // 请注意，这里的月份一定要减1，即一月应该是“0月”
+                        if (leftTime < 10000) {
+                            window.location.href = 'https://live.bilibili.com/10038913'
+                        } else {
+                            window.location.href = 'introduction.html';
+                        }
+                    },
+                    error: function () {
+                        $('#loading').text(" ")
+                        $("#errmsg").text("请检查网络链接并刷新页面");
+                    }
+                });
             }
         },
         error: function () {
-            window.alert("请检查网络链接并刷新页面");
+            $('#loading').text(" ")
+            $("#errmsg").text("请检查网络链接并刷新页面");
         }
     });
 }
