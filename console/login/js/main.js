2b68962741d055aafbc6fc4a69ac73fb4c023c7a
Bryan Chiu <1930893235@qq.com>
2020-10-07 10:43:46 +0800

    页面和结构优化

--- console/login/js/main.js
+++ console/login/js/main.js
@@ -1,15 +1,21 @@
-function submit() {
-    var um = document.getElementById("um").value;
-    var pd = document.getElementById("pd").value;
-    $.post("接受文件", {
-        username: um,
-        password: pd
-
-    },
-
-        function (data, status) {
-
-            alert("数据: \n" + data + "\n状态: " + status);
-
-        });
-}
+function load(){
+	if($.cookie('ver')=="yes"){
+	sucess();
+	}
+	}
+	function sucess(){
+		$.cookie('ver', 'yes', { expires: 7 , path: '/' });
+		window.location.href="../console.html";
+	}
+	function submita(){
+		let username = $('#um').val();
+		let password = $('#pd').val();
+		let str = username + '#' + password;
+		let ciphertext = $.md5(str);
+		if(ciphertext=="5453077ed8f5377a0319605198cd1e2a"){
+			$("#tips").text("密码正确");
+			setTimeout('sucess()', 1000);
+		}else{
+			$("#tips").text("密码或用户名错误");
+		}
+	}
