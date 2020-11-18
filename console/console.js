6b4e5d39d08dc4224c6e4a33d6640ca9405e4252
Bryan Chiu <1930893235@qq.com>
2020-11-18 16:09:47 +0800

    格式化

--- console/console.js
+++ console/console.js
@@ -8,24 +8,38 @@ function start() {
         data: {
             cmd: "start"
         },
-        success: function (data) {
+        success: function(data) {
             if (data == "success") {
                 live = true;
                 $('#LiveControlButton').text("停 止 直 播").unbind("click").click(end);
                 $("#LiveStatusReminder").css("visibility", "visible");
-                growl.show({ text: "直播已开始！", type: "custom", imgsrc: "/src/img/growl/ok.gif", autoclose: 1000 });
+                growl.show({
+                    text: "直播已开始！",
+                    type: "custom",
+                    imgsrc: "/src/img/growl/ok.gif",
+                    autoclose: 1000
+                });
             } else {
                 alert(data);
             }
         }
     })
 }
-function logOut(){
-	//登出
-	$.cookie('user', "", { expires: -1, path: '/' });
-	$.cookie('time', "", { expires: -1, path: '/' });
-	$.cookie('token', "", { expires: -1, path: '/' });
-	location.reload();
+function logOut() {
+    //登出
+    $.cookie('user', "", {
+        expires: -1,
+        path: '/'
+    });
+    $.cookie('time', "", {
+        expires: -1,
+        path: '/'
+    });
+    $.cookie('token', "", {
+        expires: -1,
+        path: '/'
+    });
+    location.reload();
 }
 function end() {
     $.ajax({
@@ -34,12 +48,17 @@ function end() {
         data: {
             cmd: "stop"
         },
-        success: function (data) {
+        success: function(data) {
             if (data == "success") {
                 live = false;
                 $("#LiveControlButton").text("开 始 直 播").unbind("click").click(start);
                 $("#LiveStatusReminder").css("visibility", "hidden");
-                growl.show({ text: "直播已停止！", type: "custom", imgsrc: "/src/img/growl/ok.gif", autoclose: 1000 });
+                growl.show({
+                    text: "直播已停止！",
+                    type: "custom",
+                    imgsrc: "/src/img/growl/ok.gif",
+                    autoclose: 1000
+                });
             } else {
                 alert(data);
             }
@@ -52,8 +71,8 @@ function get() {
     $.ajax({
         url: "/console/livestatus.json",
         method: "GET",
-        success: function (data) {
-            var live = data.Live;//是否开播
+        success: function(data) {
+            var live = data.Live; //是否开播
             if (live) {
                 $("#LiveStatusReminder").css("visibility", "visible");
                 $('#LiveControlButton').text("停 止 直 播").unbind("click").click(end);
@@ -66,6 +85,35 @@ function get() {
 }
 
 function load() {
+    //显示加载特效
+    $('#loading').show();
+    if ($.cookie('user') != null && $.cookie('token') != null && $.cookie('time') != null) {
+        //验证token
+        $.post("CHEACKIN.php", {
+            a: $.cookie('user'),
+            b: $.cookie('time'),
+            c: $.cookie('token')
+        },
+        function(result) {
+            if (result == "valid") {
+                $('#loading').hide();
+                console.log("Welcome %s, hope you can have fun in here\nyou already login %s days", $.cookie('user'), ((new Date()).getTime() - $.cookie('time')) / 86400000);
+            } else {
+                //重新登录指引
+                $("body").append("<div id='msg'><span>you should relogin</span></div>");
+                setTimeout(function() {
+                    window.location.href = "login/index.html";
+                },
+                2000);
+            }
+        });
+    } else {
+        $("body").append("<div id='msg'><span>you should relogin</span></div>");
+        setTimeout(function() {
+            window.location.href = "login/index.html";
+        },
+        2000);
+    }
     setInterval(newtime, 1000);
     get();
 }
--- console/login/js/main.js
+++ console/login/js/main.js
@@ -1,72 +1,96 @@
-	function load(){
-		//验证token
-		if($.cookie('user')!=null&&$.cookie('token')!=null&&$.cookie('time')!=null){
-			$.post("../CHEACKIN.php",{a:$.cookie('user'),b:$.cookie('time'),c:$.cookie('token')},function(result){
-				if(result=="valid"){
-					sucess($.cookie('user'));
-				}
-			});
-		}
-		//加载遮蔽罩关闭
-		$('#loading').hide();
-	}
-	document.onkeydown=function(){
-		//回车提交
-		if(event.keyCode==13)
-		{
-			$('#bu').click();
-		}
-	}
-	function sucess(name=$('#um').val()){
-		let now=(new Date()).getTime();
-		//取token 设置cookie
-		$.post("../CHEACKIN.php",{a:name,b:now,c:'1'},function(result){
-			$.cookie('user',name,{ expires: 7, path: '/' });
-			$.cookie('token', result, { expires: 7, path: '/' });
-			$.cookie('time',now,{ expires: 7, path: '/' });
-			window.location.href="../console.html";
-		});
-	}
-	function submita(){
-		//显示加载特效
-		$('#loading').show();
-		//用jq取值
-		let username = $('#um').val();
-		let password = $('#pd').val();
-		if(username==""||password==""){
-			$("#tips").text("密码或用户名不能为空");
-			$('#loading').hide();
-		}else{
-			let str = username + '#' + password;
-			let mean=1;//模式选择
-			let ciphertext='';
-			switch(mean){
-				case 1://md5加密
-				ciphertext = $.md5(str);
-				break;
-				case 2://sha256加密
-				ciphertext = sha256_digest(str);
-				break;
-			}
-			SHOWING=$.ajax({
-					type: "POST",
-					url: './Verify.php',
-					async:true,  //使用异步的方式,true为异步方式
-					data: {'PW':ciphertext,'MEAN':mean},
-					success: function (result) {
-						if(result=="success"){
-							$("#tips").text("用户名和密码正确");
-							setTimeout('sucess()', 1000);
-						}else{
-							$("#tips").text("密码或用户名错误");
-						}
-					},
-					error: function() {
-						$("#tips").text('服务器或网络异常')
-					}
-				});
-				$.when(SHOWING).done(function () {
-					$('#loading').hide();
-				});
-		}
-	}
+function load() {
+    //验证token
+    if ($.cookie('user') != null && $.cookie('token') != null && $.cookie('time') != null) {
+        $.post("../CHEACKIN.php", {
+            a: $.cookie('user'),
+            b: $.cookie('time'),
+            c: $.cookie('token')
+        },
+        function(result) {
+            if (result == "valid") {
+                sucess($.cookie('user'));
+            }
+        });
+    }
+    //加载遮蔽罩关闭
+    $('#loading').hide();
+}
+document.onkeydown = function() {
+    //回车提交
+    if (event.keyCode == 13) {
+        $('#bu').click();
+    }
+}
+function sucess(name = $('#um').val()) {
+    let now = (new Date()).getTime();
+    //取token 设置cookie
+    $.post("../CHEACKIN.php", {
+        a: name,
+        b: now,
+        c: '1'
+    },
+    function(result) {
+        $.cookie('user', name, {
+            expires: 7,
+            path: '/'
+        });
+        $.cookie('token', result, {
+            expires: 7,
+            path: '/'
+        });
+        $.cookie('time', now, {
+            expires: 7,
+            path: '/'
+        });
+        window.location.href = "../console.html";
+    });
+}
+function submita() {
+    //显示加载特效
+    $('#loading').show();
+    //用jq取值
+    let username = $('#um').val();
+    let password = $('#pd').val();
+    if (username == "" || password == "") {
+        $("#tips").text("密码或用户名不能为空");
+        $('#loading').hide();
+    } else {
+        let str = username + '#' + password;
+        let mean = 1; //模式选择
+        let ciphertext = '';
+        switch (mean) {
+        case 1:
+            //md5加密
+            ciphertext = $.md5(str);
+            break;
+        case 2:
+            //sha256加密
+            ciphertext = sha256_digest(str);
+            break;
+        }
+        SHOWING = $.ajax({
+            type: "POST",
+            url: './Verify.php',
+            async: true,
+            //使用异步的方式,true为异步方式
+            data: {
+                'PW': ciphertext,
+                'MEAN': mean
+            },
+            success: function(result) {
+                if (result == "success") {
+                    $("#tips").text("用户名和密码正确");
+                    setTimeout('sucess()', 1000);
+                } else {
+                    $("#tips").text("密码或用户名错误");
+                }
+            },
+            error: function() {
+                $("#tips").text('服务器或网络异常')
+            }
+        });
+        $.when(SHOWING).done(function() {
+            $('#loading').hide();
+        });
+    }
+}
